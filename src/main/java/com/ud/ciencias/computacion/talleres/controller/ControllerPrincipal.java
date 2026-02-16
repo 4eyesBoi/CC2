1e4fb476cb6bf214f112ba16f5f4bfd07d3c0039
4eyesBoi <minuevacu@gmail.com>
2026-02-15 22:31:47 -0500

    Added initial functions of creating array

--- src/main/java/com/ud/ciencias/computacion/talleres/controller/ControllerPrincipal.java
+++ src/main/java/com/ud/ciencias/computacion/talleres/controller/ControllerPrincipal.java
@@ -3,25 +3,40 @@ package com.ud.ciencias.computacion.talleres.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ud.ciencias.computacion.talleres.services.CrearService;
 
 
 
 @Controller
 public class ControllerPrincipal {
 
-    
+    private CrearService crearService;
+
+    public ControllerPrincipal(CrearService crearService) {
+        this.crearService = crearService;
+    }
 
     @GetMapping("/")
     public String home() {
         return "index"; // templates/index.html
     }
     
-    @GetMapping("/servicios/activos")
+    @GetMapping("/arreglo")
     public String serviciosActivos() {
 
 
-        System.out.println("Ingrese servicios activos");
-        return "prueba"; // templates/prueba.html
+        System.out.println("hola arreglo");
+        return "prueba"; // templates/index.html
+    }
+
+    @PostMapping("/crearArreglo")
+    public String crearArreglo(@RequestParam("tamArr") int tamArr, @RequestParam("tamClav") int tamClav) {
+        
+        this.crearService.crearArreglo(tamArr, tamClav);
+        return "prueba"; // templates/resultado.html
     }
 }
 
--- src/main/java/com/ud/ciencias/computacion/talleres/services/CrearService.java
+++ src/main/java/com/ud/ciencias/computacion/talleres/services/CrearService.java
@@ -1,6 +1,14 @@
 package com.ud.ciencias.computacion.talleres.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CrearService {
-    //hola
-    //hola
+    
+
+    public void crearArreglo(int tamArr, int tamClav) {
+        System.out.println("creando arreglo con tamaño: " + tamArr + " y clave de tamaño: " + tamClav);
+        int[] arreglo = new int[tamArr]; // Crear un arreglo de enteros con tamaño tamArr
+        System.out.println("Arreglo creado: " + java.util.Arrays.toString(arreglo));
+    }
 }
