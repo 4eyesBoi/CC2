48de92e8e96790edcbfb8a8d00ba5fc0e3664cd3
Jorge Eduardo Muñoz Gomez <topaga1709@gmail.com>
2026-02-22 21:07:34 -0500

    Agregadas mis modificaciones al proyecto

--- src/main/java/com/ud/ciencias/computacion/talleres/controller/ControllerPrincipal.java
+++ src/main/java/com/ud/ciencias/computacion/talleres/controller/ControllerPrincipal.java
@@ -3,40 +3,43 @@ package com.ud.ciencias.computacion.talleres.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.ud.ciencias.computacion.talleres.services.CrearService;
 
 
 
 @Controller
 public class ControllerPrincipal {
 
-    private CrearService crearService;
-
-    public ControllerPrincipal(CrearService crearService) {
-        this.crearService = crearService;
-    }
+    
 
     @GetMapping("/")
     public String home() {
         return "index"; // templates/index.html
     }
     
-    @GetMapping("/arreglo")
+    @GetMapping("/servicios/activos")
     public String serviciosActivos() {
 
 
-        System.out.println("hola arreglo");
-        return "prueba"; // templates/index.html
+        System.out.println("Ingrese servicios activos");
+        return "prueba"; // templates/prueba.html
     }
 
-    @PostMapping("/crearArreglo")
-    public String crearArreglo(@RequestParam("tamArr") int tamArr, @RequestParam("tamClav") int tamClav) {
-        
-        this.crearService.crearArreglo(tamArr, tamClav);
-        return "prueba"; // templates/resultado.html
+    @GetMapping("/busquedas/lineal")
+    public String busquedasLineal() {
+        System.out.println("Accediendo a Busquedas - Lineal");
+        return "busquedas-lineal"; // templates/busquedas-lineal.html
+    }
+
+    @GetMapping("/busquedas/binaria")
+    public String busquedasBinaria() {
+        System.out.println("Accediendo a Busquedas - Binaria");
+        return "busquedas-binaria"; // templates/busquedas-binaria.html
+    }
+
+    @GetMapping("/busquedas/hash")
+    public String busquedasHash() {
+        System.out.println("Accediendo a Busquedas - Funciones Hash");
+        return "busquedas-hash"; // templates/busquedas-hash.html
     }
 }
 
--- src/main/java/com/ud/ciencias/computacion/talleres/services/CrearService.java
+++ src/main/java/com/ud/ciencias/computacion/talleres/services/CrearService.java
@@ -1,14 +1,6 @@
 package com.ud.ciencias.computacion.talleres.services;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public class CrearService {
-    
-
-    public void crearArreglo(int tamArr, int tamClav) {
-        System.out.println("creando arreglo con tamaño: " + tamArr + " y clave de tamaño: " + tamClav);
-        int[] arreglo = new int[tamArr]; // Crear un arreglo de enteros con tamaño tamArr
-        System.out.println("Arreglo creado: " + java.util.Arrays.toString(arreglo));
-    }
+    //hola
+    //hola
 }
